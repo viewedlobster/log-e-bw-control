942b6c718fbabdcefec76f718bee28fbf830c440
Ellen Arvidsson <ellen@log-e.se>
2025-03-16 23:49:05 +0100

    copyright notice

--- src/main/java/se/loge/bwcontrol/common/SysexBuilder.java
+++ src/main/java/se/loge/bwcontrol/common/SysexBuilder.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright (C) 2025 Ellen Arvidsson
+ *
+ * This file is part of log-e-bw-control.
+ *
+ * log-e-bw-control is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * log-e-bw-control is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with log-e-bw-control. If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
 package se.loge.bwcontrol.common;
 
 import java.util.ArrayList;
@@ -9,13 +29,8 @@ public class SysexBuilder {
     data = new ArrayList<>();
   }
 
-  public void addRaw(byte[] b) {
-    for (int i = 0; i < b.length; i++) {
-      data.add(b[i]);
-    }
-  }
-
   public void add(int b) {
+    // TODO: reenable check?
     //if  (0 > b || b > 0x7f) {
       //throw new HWError(
         //String.format("error: couldn't convert integer %x to 7 bit byte in sysex bilder", b));
@@ -53,7 +68,12 @@ public class SysexBuilder {
   public static int lsb(int v) {
     return (v & 0x7f);
   }
-  
+
+  /* borrowed this from
+   * https://stackoverflow.com/questions/9655181/java-convert-a-byte-array-to-a-hex-string
+   * 
+   * it's not difficult, just couldn't be arsed
+   */
   private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
   public static String bytesToHexString(byte[] bytes) {
     char[] hexChars = new char[bytes.length * 2];
@@ -63,6 +83,6 @@ public class SysexBuilder {
         hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
     }
     return new String(hexChars);
-}
+  }
 
 }
