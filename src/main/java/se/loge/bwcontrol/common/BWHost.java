ef8d40000afe218718596e7b9c58db05976f8c4e
Ellen Arvidsson <ellen@log-e.se>
2025-04-02 12:22:23 +0200

    cleanup and license stuff

--- src/main/java/se/loge/bwcontrol/common/BWHost.java
+++ src/main/java/se/loge/bwcontrol/common/BWHost.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright (C) 2025 Ellen Arvidsson
+ *
+ * This file is part of log-e-bw-control.
+ *
+ * log-e-bw-control is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * log-e-bw-control is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with log-e-bw-control. If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
 package se.loge.bwcontrol.common;
 
 import java.util.LinkedList;
